886d8e6d0e7049b40b9f9cc481e6ea3f35056e21
ioanacl-filip <filipioanaclaudia@yahoo.ro>
2024-11-25 00:08:25 +0200

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FilipIoanaLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
